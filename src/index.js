771475fa3bf89d8e9a6bb79a59d92f5d1d0ee087
ispirett <ispirett@gmail.com>
2019-09-18 14:07:18 -0400

    feature(winning logic): added winning casses and winning position funttions

--- src/index.js
+++ src/index.js
@@ -18,10 +18,48 @@ function titTacToe () {
        return{name, icon}
    };
 
-   let nameOne = window.prompt("PlayerOne name");
-   let nameTWo = window.prompt("PlayerTwo name ");
+   let nameOne = "hi";//window.prompt("PlayerOne name");
+   let nameTWo = "bye" ;//window.prompt("PlayerTwo name ");
    const playerOne = Player(nameOne,icon );
-    const playerTwo = Player(nameOne,iconTwo );
+   const playerTwo = Player(nameOne,iconTwo );
+
+
+    const winningCases = (array) => {
+
+        array.flat().forEach(boxArray=>{
+            console.log(array.flat());
+            if(boxArray.every(e => e.innerHTML === 'X')){
+
+                alert('Winner' + selectPlayer().name())
+            }
+
+            if(boxArray.every(e => e.innerHTML === 'O')){
+                alert('Winner' + selectPlayer().name())
+
+            }
+
+        })
+
+    };
+
+    const winningPositions = (b) =>{
+        const horizontal = [[b[0],b[1],b[2]],
+                            [b[3],b[4],b[5]],
+                            [b[6],b[7],b[8]]
+                           ];
+        const vertical = [[b[0],b[3],b[6]],
+                          [b[1],b[4],b[7]],
+                          [b[2],b[5],b[8]]
+                         ];
+        const diagonal =  [[b[0],b[4],b[8]],
+                          [b[2],b[4],b[6]]
+
+                        ];
+
+
+        return [horizontal, vertical, diagonal]
+    };
+
 
    const selectPlayer = () => {
         if(turn % 2 !== 0) return playerOne;
@@ -31,8 +69,8 @@ function titTacToe () {
    const playerTurn = (element) => {
        
        element.target.innerHTML =  selectPlayer().icon();
-       turn -= 1
-       gameOver(selectPlayer().name())
+       turn -= 1;
+      /// gameOver(selectPlayer().name());
        console.log(turn)
    };
 
@@ -42,22 +80,27 @@ function titTacToe () {
        setTimeout(() => {
             msg.innerText = ''
        }, 4000)
-   }
+   };
 
-   const gameOver = (playerName) => {
-        GameOver = true;
+   const gameOver = (playerName, over) => {
+       GameOver = over;
         if(turn === 0) {
             alert("It's a draw")
         }
+
         alert(playerName + " won the game");
-   }
+   };
 
    const boxes = document.querySelectorAll('.box');
 
     boxes.forEach((box, index)=>{
         box.onclick = (element) =>{
-            playerTurn(element)
+            playerTurn(element);
             displayMessage(element.target.innerText);
+
+            // Checking winning positions
+            const positions = winningPositions(boxes);
+            winningCases(positions);
         }
 
     })
