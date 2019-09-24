8ed6cd88c39b1c73deec1a4d5b4665d6362b5e78
Torres-ssf <sergio.uft@gmail.com>
2019-09-24 15:27:25 -0300

    feature(stickler): fix some stickler errors

--- src/player.js
+++ src/player.js
@@ -1,52 +1,52 @@
 // Player
 
 const playerManager = (() => {
-    const Player = (playerName, playerIcon) => {
-        const name = () => playerName;
-        const icon = () => playerIcon;
-        return { name, icon };
+  const Player = (playerName, playerIcon) => {
+    const name = () => playerName;
+    const icon = () => playerIcon;
+    return { name, icon };
+  };
+
+  const selectPlayer = (turn, playerOne, playerTwo) => {
+    if (turn % 2 !== 0) return playerOne;
+    return playerTwo;
+  };
+
+  const getPlayerNames = (() => {
+    const playerOneName = () => {
+      let nameOne = window.prompt("PlayerOne name");
+      if (nameOne === "") return playerOneName();
+      else if (nameOne === null) {
+        return "Greatest-loser-ever";
+      }
+      return nameOne;
     };
 
-    const selectPlayer = (turn, playerOne, playerTwo) => {
-        if (turn % 2 !== 0) return playerOne;
-        return playerTwo;
+    const playerTwoName = callback => {
+      let nameTwo = window.prompt("PlayerTwo name");
+      if (nameTwo === "") return playerTwoName();
+      else if (nameTwo === null) {
+        try {
+          callback();
+        } catch (e) { }
+        return "Loser-For-Sure";
+      }
+      try {
+        callback();
+      } catch (e) { }
+      return nameTwo;
     };
-
-    const getPlayerNames = (() => {
-        const playerOneName = () => {
-            let nameOne = window.prompt("PlayerOne name");
-            if (nameOne === "") return playerOneName();
-            else if (nameOne === null) {
-                return "Greatest-loser-ever";
-            }
-            return nameOne;
-        };
-
-        const playerTwoName = callback => {
-            let nameTwo = window.prompt("PlayerTwo name");
-            if (nameTwo === "") return playerTwoName();
-            else if (nameTwo === null) {
-                try {
-                    callback();
-                } catch (e) { }
-                return "Loser-For-Sure";
-            }
-            try {
-                callback();
-            } catch (e) { }
-            return nameTwo;
-        };
-
-        return {
-            playerOneName,
-            playerTwoName
-        };
-    })();
 
     return {
-        Player,
-        selectPlayer,
-        getPlayerNames
+      playerOneName,
+      playerTwoName
     };
+  })();
+
+  return {
+    Player,
+    selectPlayer,
+    getPlayerNames
+  };
 })();
 export default playerManager;
