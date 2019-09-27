e87879fec292f7624b68008bb650cacab4daee57
Torres-ssf <sergio.uft@gmail.com>
2019-09-26 22:18:05 -0300

    chore(player): Refactor player module

--- src/player.js
+++ src/player.js
@@ -9,32 +9,10 @@ const playerManager = (() => {
     if (turn % 2 !== 0) return playerOne;
     return playerTwo;
   };
-  const getPlayerNames = (() => {
-    const playerOneName = () => {
-      const nameOne = 'a' //window.prompt('PlayerOne name');
-      if (nameOne === '') return playerOneName();
-      else if (nameOne === null) {
-        return 'Player-1';
-      }
-      return nameOne;
-    };
-    const playerTwoName = (() => {
-      const nameTwo = 'b' //window.prompt('PlayerTwo name');
-      if (nameTwo === "") return playerTwoName();
-      else if (nameTwo === null) {
-        return 'Player-2';
-      }
-      return nameTwo;
-    });
-    return {
-      playerOneName,
-      playerTwoName,
-    };
-  })();
+
   return {
     Player,
     selectPlayer,
-    getPlayerNames,
   };
 })();
 export default playerManager;
